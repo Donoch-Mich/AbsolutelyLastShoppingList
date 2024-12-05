fcb3a74aa398795b500b406e301cd9d885be09ed
java <java@VISUAL-STUDIO-C>
2024-12-05 17:40:40 +0100

    Add project files.

---
+++ AbsolutelyLastShoppingList/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AbsolutelyLastShoppingList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
